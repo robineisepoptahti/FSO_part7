c28b23a2a2668e229804dcae4fe4870e157acb93
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-03-23 19:08:55 +0200

    Added 7.6(again)

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -70,9 +70,9 @@ const Footer = () => (
 
 const CreateNew = (props) => {
 
-  const content = useField('content')
-  const author = useField('author')
-  const info = useField('info')
+  const { reset: resetContent, ...content} = useField('content')
+  const { reset: resetAuthor, ...author } = useField('author')
+  const { reset: resetInfo, ...info } = useField('info')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -86,9 +86,9 @@ const CreateNew = (props) => {
 
   const resetFields = (event) => {
     event.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
@@ -146,7 +146,7 @@ const App = () => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
     navigate('/anecdotes')
-    msg(`A new anecdote'${anecdote.content}' was created.`)
+    msg(`A new anecdote '${anecdote.content}' was created.`)
   }
 
   const anecdoteById = (id) =>
